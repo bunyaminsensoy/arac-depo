2c348686cf6883865875b31f346335545c871a51
ibrahimuyumaz <ibrahimuyumaz001@gmail.com>
2022-05-31 17:52:48 +0300

    .

--- Menu.py
+++ Menu.py
@@ -11,3 +11,21 @@ class Menu:
         cycle = True
         print("Kayit eklemek icin :1\n""Kayit okumak icin :2\n"
               "Arac bilgilerini gormek icin :3\n" "Programi sonlandirmak icin :4\n")
+
+        while( cycle ):     
+            try:
+                choose = input("Lutfen yapmak istediginiz islemi secin: ").strip()
+                while not(choose.isnumeric()):
+                    choose = input("Gecersiz bir komut girildi, tekrar deneyin: ")
+            except Exception as e:
+                print("Oops! ", e.__class__, " hatasi meydana geldi!")
+            if choose == '1':
+                self.addRecord()
+            if choose == '2':
+                self.readRecords()
+            if choose == '3':
+                self.getVehicle()
+            if choose == '4':
+                print("Programdan cikiliyor..")
+                cycle = False
+            print()
