40c716a73ebbe4c54433947c1c5540df88836ef0
ibrahimode <100562789+ibrahimode@users.noreply.github.com>
2022-05-31 18:35:24 +0300

    ,

--- Vehicle.py
+++ Vehicle.py
@@ -5,3 +5,8 @@ class Vehicle:
     
     def __init__(self, plaka="None"):
         self.__plaka = plaka
+
+        self.__aracModeli = input("Aracinizin modelini giriniz: ").strip().upper()
+        while not(self.aracModeli.isalpha()):
+            print("Arac modeli sadece alfabeden olusabilir!!")
+            self.__aracModeli = input("Aracinizin modelini giriniz: ").strip().upper()
