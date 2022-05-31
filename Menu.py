bdf287bcbf38bc7fb73f2853186dfac7b5e8c949
ibrahimuyumaz <ibrahimuyumaz001@gmail.com>
2022-05-31 17:50:25 +0300

    .

--- Menu.py
+++ Menu.py
@@ -6,3 +6,8 @@ class Menu:
         print("Menuye hosgeldiniz diyelim ")
         self.vehicleList = {}
         self.printRecord = []
+
+    def menu(self):
+        cycle = True
+        print("Kayit eklemek icin :1\n""Kayit okumak icin :2\n"
+              "Arac bilgilerini gormek icin :3\n" "Programi sonlandirmak icin :4\n")
