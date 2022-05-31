7268603f4af06aef71cf700c04418358027af2a0
ibrahimuyumaz <ibrahimuyumaz001@gmail.com>
2022-05-31 17:49:24 +0300

    menü class

--- Menu.py
+++ Menu.py
@@ -1,2 +1,8 @@
 from Vehicle import Vehicle
 import pandas as pd
+
+class Menu:
+    def __init__(self):
+        print("Menuye hosgeldiniz diyelim ")
+        self.vehicleList = {}
+        self.printRecord = []
